fb9d3b0b9e49b22ca650bb8b7d44f033389da43f
sionkim0126 <kimsion000@naver.com>
2025-07-30 11:55:07 +0900

    프로젝트pro06 / (서블릿 기초)  Get/Post 방식 요청 동시에 처리

---
+++ pro06/src/main/java/sec01/ex01/LoginServlet4.java
@@ -0,0 +1,56 @@
+package sec01.ex01;
+
+import java.io.IOException;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet4
+ */
+@WebServlet("/login4")
+public class LoginServlet4 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		System.out.println("LoginServlet4 init메서드 호출");
+	}
+
+	/**
+	 * @see Servlet#destroy()
+	 */
+	public void destroy() {
+		System.out.println("LoginServlet4 destroy메서드 호출");
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("LoginServlet4 doGet 메서드 호출");
+		doHandle(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("LoginServlet4 doPost 메서드 호출");
+		doHandle(request, response);
+	}
+	
+	private void doHandle(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+		request.setCharacterEncoding("utf-8");
+		String user_id = request.getParameter("user_id");
+		String user_pw = request.getParameter("user_pw");
+		System.out.println("LoginServlet4 doHandle 메서드 호출");
+		System.out.println("아이디 : " + user_id);
+		System.out.println("비 번 : " + user_pw);
+	}
+}
