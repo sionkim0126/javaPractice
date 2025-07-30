a602cd70c254ea7c25802220c6a07599be2f572b
sionkim0126 <kimsion000@naver.com>
2025-07-30 11:53:53 +0900

    프로젝트pro06 / (서블릿 기초)  post 방식으로 요청

---
+++ pro06/src/main/java/sec01/ex01/LoginServlet3.java
@@ -0,0 +1,44 @@
+package sec01.ex01;
+
+import java.io.IOException;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet3
+ */
+@WebServlet("/login3")
+public class LoginServlet3 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		System.out.println("LoginServlet3 init메서드 호출");
+	}
+
+	/**
+	 * @see Servlet#destroy()
+	 */
+	public void destroy() {
+		System.out.println("LoginServlet3 destroy메서드 호출");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		String user_id = request.getParameter("user_id");
+		String user_pw = request.getParameter("user_pw");
+		System.out.println("아이디 : "+ user_id);
+		System.out.println("비 번 : "+ user_pw);
+		
+	}
+
+}
