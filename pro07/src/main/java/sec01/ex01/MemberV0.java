fb39bad86f33b4d5fd3ced6a9fdcfa0d25adefcd
sionkim0126 <kimsion000@naver.com>
2025-08-04 18:10:43 +0900

    프로젝트pro07 / (서블릿 비지니스 로직 처리)

---
+++ pro07/src/main/java/sec01/ex01/MemberV0.java
@@ -0,0 +1,47 @@
+package sec01.ex01;
+
+import java.sql.Date;
+
+public class MemberV0 {
+	private String id;
+	private String pwd;
+	private String name;
+	private String email;
+	private Date joinDate;
+	
+	public MemberV0(){
+		System.out.println("MemberV0 생성자 호출");
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPwd() {
+		return pwd;
+	}
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Date getJoinDate() {
+		return joinDate;
+	}
+	public void setJoinDate(Date joinDate) {
+		this.joinDate = joinDate;
+	}
+	
+
+}
