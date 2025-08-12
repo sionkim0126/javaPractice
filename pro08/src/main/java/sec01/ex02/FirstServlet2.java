0171d1b30d553192f4acf2cbde7f56f175a24286
sionkim0126 <kimsion000@naver.com>
2025-08-12 17:54:37 +0900

    프로젝트pro08 / (서블릿 확장 API 사용)  Refresh 이용한 포워딩

---
+++ pro08/src/main/java/sec01/ex02/FirstServlet2.java
@@ -0,0 +1,29 @@
+package sec01.ex02;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class RefreshServlet
+ */
+@WebServlet("/first2")
+public class FirstServlet2 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html; charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		
+		response.addHeader("Refresh", "5;url=second2");//웹 브라우저에 5초 후 서블릿 refreshSecond로 재요청
+	}
+
+}
