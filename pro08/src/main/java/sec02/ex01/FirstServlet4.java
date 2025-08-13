4826c17ab3ece011f00352f41cd24648c875509b
sionkim0126 <kimsion000@naver.com>
2025-08-13 15:16:59 +0900

    프로젝트pro08 / (서블릿 확장 API 사용)  Redirect방식으로 다른 서블릿에 데이터 전달하기

---
+++ pro08/src/main/java/sec02/ex01/FirstServlet4.java
@@ -0,0 +1,29 @@
+package sec02.ex01;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class FirstServlet4
+ */
+@WebServlet("/first4")
+public class FirstServlet4 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html; charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		response.sendRedirect("second4?name=lee");
+	}
+
+}
