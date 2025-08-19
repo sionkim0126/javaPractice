0983fc0b3e30beaa5dc95e3532566a3a1103d94e
sionkim0126 <kimsion000@naver.com>
2025-08-19 13:37:11 +0900

    프로젝트pro08 / (서블릿 확장 API 사용)  ServletContext의 파일 입출력 기능

---
+++ pro08/src/main/java/sec05/ex03/ContextFileServlet.java
@@ -0,0 +1,53 @@
+package sec05.ex03;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ContextFileServlet
+ */
+@WebServlet("/cfile")
+public class ContextFileServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html; charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		ServletContext context = getServletContext();
+		InputStream is = context.getResourceAsStream("/WEB-INF/bin/init.txt"); //해당 파일을 읽어옵니다.
+		BufferedReader buffer = new BufferedReader(new InputStreamReader(is));
+		
+		String menu = null;
+		String menu_member = null;
+		String menu_order = null;
+		String menu_goods = null;
+		while((menu = buffer.readLine()) != null) {
+			StringTokenizer tokens = new StringTokenizer(menu, ","); //콤마를 구분자로 하여 메뉴 항목을 분리합니다.
+			menu_member = tokens.nextToken();
+			menu_order = tokens.nextToken();
+			menu_goods = tokens.nextToken();
+			}
+		
+		out.print("<html><body>");
+		out.print( menu_member  + "<br>");
+		out.print( menu_order  + "<br>");
+		out.print( menu_goods  + "<br>");
+		out.print("</body></html>");
+		out.close();
+	}
+
+}
