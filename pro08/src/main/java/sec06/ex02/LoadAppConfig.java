ef0c8b8096fbc49376af5b8de268e8ded52c8ba1
sionkim0126 <kimsion000@naver.com>
2025-08-19 18:13:00 +0900

    프로젝트pro08 / (서블릿 확장 API 사용) Load-On-Startup기능 사용 (애너테이션 / web.xml)

---
+++ pro08/src/main/java/sec06/ex02/LoadAppConfig.java
@@ -0,0 +1,57 @@
+package sec06.ex02;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoadAppConfig
+ */
+/*@WebServlet(name= "loadConfig",urlPatterns = { "/loadConfig" }, loadOnStartup = 1) */ //loadOnstartup 속성을 추가하고 우선순위를 1로 설정합니다. 
+public class LoadAppConfig extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+		private ServletContext context;
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		System.out.println("LoadAppConfig의 init 메서드 호출");
+		context = config.getServletContext();
+		String menu_member = context.getInitParameter("menu_member"); //web.xml에 설정되어있는 매개변수를 읽어드립니다.
+		String menu_order = context.getInitParameter("menu_order");
+		String menu_goods = context.getInitParameter("menu_goods");
+		
+		context.setAttribute("menu_member", menu_member);
+		context.setAttribute("menu_order", menu_order);
+		context.setAttribute("menu_goods", menu_goods);
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html; charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		
+		String menu_member = (String)context.getAttribute("menu_member");
+		String menu_order = (String)context.getAttribute("menu_order");
+		String menu_goods = (String)context.getAttribute("menu_goods");
+		
+		out.print("<html><body>");
+		out.print("<table border=1 cellspacing=0 ><tr>메뉴 이름</tr>");
+		out.print("<tr><td>" + menu_member + "</td></tr>");
+		out.print("<tr><td>" + menu_order + "</td></tr>");
+		out.print("<tr><td>" + menu_goods + "</td></tr>");
+		out.print("</table></body></html>");
+		
+	}
+
+}
