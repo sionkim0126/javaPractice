cdab5d718f478474d57f17156133a47b439563ec
sionkim0126 <kimsion000@naver.com>
2025-11-25 17:05:33 +0900

    프로젝트pro10 / (서블릿의 필터와 리스너 기능)  Logout기능 작업 중지

---
+++ pro10/src/main/java/sec04/ex02/LoginTest3.java
@@ -0,0 +1,52 @@
+package sec04.ex02;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import sec04.ex01.LoginImpl;
+
+/**
+ * Servlet implementation class LoginTest3
+ */
+@WebServlet("/login3")
+public class LoginTest3 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	ServletContext context = null;
+	List<Object> user_list = new ArrayList<>();
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html; charset=UTF-8");
+		
+		PrintWriter out = response.getWriter();
+		context = getServletContext();
+		HttpSession session = request.getSession();
+		
+		String user_id = request.getParameter("user_id");
+		String user_pw = request.getParameter("user_pw");
+		
+		LoginImpl loginUser = new LoginImpl(user_id, user_pw);
+		if(session.isNew()) {
+			session.setAttribute("loginUser", loginUser);
+			user_list.add(user_id);
+			context.setAttribute("user_list", user_list);
+			
+		}
+		
+		
+		
+	}
+
+}
