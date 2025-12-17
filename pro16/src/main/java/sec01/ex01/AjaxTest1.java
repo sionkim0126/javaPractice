13ea25edc19613603720d754b9497295ddb92816
sionkim0126 <kimsion000@naver.com>
2025-12-17 17:28:35 +0900

    프로젝트pro16 / (HTML5와 JQuery) jQuery Ajax 기능 사용

---
+++ pro16/src/main/java/sec01/ex01/AjaxTest1.java
@@ -0,0 +1,44 @@
+package sec01.ex01;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class AjaxTest1
+ */
+@WebServlet("/ajaxTest1")
+public class AjaxTest1 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doHandle(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doHandle(request, response);
+	}
+	
+	private void doHandle(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+		request.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html; charset=UTF-8");
+		
+		String param = (String)request.getParameter("param");
+		System.out.println("param : " + param);
+		//getParameter 메서드를 이용해 ajax로 전송된 매개변수를 가져옵니다.
+		
+		PrintWriter out = response.getWriter();
+		out.print("안녕하십니까 '서버'입니다.");
+	}
+
+}
