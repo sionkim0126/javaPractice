2cee4e359e82db69915ad256723f8cf91abebb51
sionkim0126 <kimsion000@naver.com>
2025-12-18 16:37:06 +0900

    프로젝트pro16 / (HTML5와 JQuery) jQuery Ajax ID 중복 체크 기능 구현

---
+++ pro16/src/main/java/sec02/ex01/MemberServlet.java
@@ -0,0 +1,57 @@
+package sec02.ex01;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MemberServlet
+ */
+@WebServlet("/mem")
+public class MemberServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MemberServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doHandle(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doHandle(request, response);
+	}
+	
+	private void doHandle(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html; charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		String id = (String)request.getParameter("id");
+		System.out.println("id : " + id);
+		
+		MemberDAO dao = new MemberDAO();
+		boolean overlappedID = dao.overlappedID(id);
+		
+		if(overlappedID == true) {
+			out.print("not_usable");
+		}else {
+			out.print("usable");
+		}
+	}
+}
