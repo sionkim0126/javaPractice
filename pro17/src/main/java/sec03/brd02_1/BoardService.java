504ea0904352be08a4e46ec61486fb97c51c8490
sionkim0126 <kimsion000@naver.com>
2026-01-07 01:22:05 +0900

    프로젝트pro17 / (모델2 방식 개발) refactor:게시판 글쓰기/파일업로드 로직 재구현 [재구현 목적] - 기존 '글쓰기 및 파일 업로드' 기능 로직 흐름을 완전히 이해하기 위해 재구현함.

---
+++ pro17/src/main/java/sec03/brd02_1/BoardService.java
@@ -0,0 +1,21 @@
+package sec03.brd02_1;
+
+import java.util.List;
+
+public class BoardService {
+	BoardDAO boardDAO;
+
+	public BoardService() {
+		boardDAO = new BoardDAO();
+	}
+	
+	public List<ArticleDTO> listArticles(){
+		List<ArticleDTO> articlesList = boardDAO.selectAllArticles();
+		return articlesList;
+	}
+	
+	public void addArticle(ArticleDTO article) {
+		boardDAO.insertNewArticle(article);
+	}
+
+}
